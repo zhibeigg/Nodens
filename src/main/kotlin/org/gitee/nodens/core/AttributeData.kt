d81c1eaada082388190c80ab16c5b2c3326b41aa
zhibei <785740487@qq.com>
2025-10-26 00:40:46 +0800

    feat(core): 为AttributeData类添加toString方法
    refactor(core): 在AttributeData类中重写toString方法以提供更详细的属性信息

--- src/main/kotlin/org/gitee/nodens/core/AttributeData.kt
+++ src/main/kotlin/org/gitee/nodens/core/AttributeData.kt
@@ -2,4 +2,9 @@ package org.gitee.nodens.core
 
 import org.gitee.nodens.common.DigitalParser
 
-class AttributeData(override val attributeNumber: IAttributeGroup.Number, override val value: DigitalParser.Value): IAttributeData
+class AttributeData(override val attributeNumber: IAttributeGroup.Number, override val value: DigitalParser.Value): IAttributeData {
+
+    override fun toString(): String {
+        return "attributeNumber=${attributeNumber.group.name}:${attributeNumber.name}, type=${value.type}, value=${value.doubleArray}"
+    }
+}
