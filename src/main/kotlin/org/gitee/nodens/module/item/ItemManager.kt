91d717221d5d06ece0ea435d6aa88241dfb39457
zhibei <785740487@qq.com>
2025-11-03 10:29:57 +0800

    fix(item): 在held事件中添加了enableArmourers检查 refactor(item): 将updateInventory中的条件反转以提前返回

--- src/main/kotlin/org/gitee/nodens/module/item/ItemManager.kt
+++ src/main/kotlin/org/gitee/nodens/module/item/ItemManager.kt
@@ -71,6 +71,7 @@ object ItemManager {
 
     @SubscribeEvent
     private fun held(e: PlayerItemHeldEvent) {
+        if (!enableArmourers) return
         val list = mutableListOf<String>()
         e.player.inventory.getItem(e.newSlot)?.getConfig()?.armourers?.let { list += it }
         e.player.inventory.itemInOffHand.getConfig()?.armourers?.let { list += it }
@@ -141,17 +142,16 @@ object ItemManager {
     }
 
     private fun updateInventory(player: Player) {
-        if (dragonCoreIsEnabled) {
-            dragoncoreSlots.forEach {
-                val item = SlotAPI.getCacheSlotItem(player, it)
-                if (item.isAir()) return
-                val context = item.context() ?: return
-                val config = getItemConfig(context.key) ?: return
-                if (config.isUpdate && config.hashCode != context.hashcode) {
-                    val new = updateItem(player, item)
-                    SlotAPI.setSlotItem(player, it, new, false)
-                    NodensItemUpdateEvents.Post(item, new).call()
-                }
+        if (!dragonCoreIsEnabled) return
+        dragoncoreSlots.forEach {
+            val item = SlotAPI.getCacheSlotItem(player, it)
+            if (item.isAir()) return
+            val context = item.context() ?: return
+            val config = getItemConfig(context.key) ?: return
+            if (config.isUpdate && config.hashCode != context.hashcode) {
+                val new = updateItem(player, item)
+                SlotAPI.setSlotItem(player, it, new, false)
+                NodensItemUpdateEvents.Post(item, new).call()
             }
         }
         updateBukkitInventory(player)
